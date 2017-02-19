af96efdc9a8de1b138377e059c3739a2c1a63adc
Yngve Leyn <yngve.leyn@gmail.com>
2017-02-19 13:25:00 +0100

    First prototype: yes/no

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
-function components(parameter1, parameter2, parameter3) {
-	// insert code here
-};function main(parameter1, parameter2, parameter3) {
-	// insert code here
-} 
+//----------------------------------------- Wheel of fortune
+
+
+//init
+function wof_init(){
+	//choose random number between 1 and 10
+	var random_deg = Math.floor((Math.random() * 10000) + 1);
+	console.log(random_deg);
+    document.getElementById('wof__wheel').style.cssText = 'transform: rotate('+random_deg+'deg)';
+};
--- js/modules/main.js
+++
@@ -1,3 +0,0 @@
-function main(parameter1, parameter2, parameter3) {
-	// insert code here
-} 
